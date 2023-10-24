9efa1114103d06bc653ecbfde6ea99e188b85e28
oldenburg.lukas <oldenburg.lukas@student.htlwrn.ac.at>
2023-10-24 10:52:41 +0200

    e5 complete

--- Exercise 5/LinqQuiz.Library/LinqQuiz.Library/Quiz.cs
+++ Exercise 5/LinqQuiz.Library/LinqQuiz.Library/Quiz.cs
@@ -11,7 +11,7 @@ public static class Quiz
     /// </exception>
     public static int[] GetEvenNumbers(int exclusiveUpperLimit)
     {
-        return Enumerable.Range(1, (exclusiveUpperLimit >= 1) ? exclusiveUpperLimit - 1 : throw new ArgumentOutOfRangeException($"{exclusiveUpperLimit} is lower than 1")).Where(x => int.IsEvenInteger(x)).ToArray();
+        return Enumerable.Range(1, (exclusiveUpperLimit >= 1) ? exclusiveUpperLimit - 1 : throw new ArgumentOutOfRangeException("Argument must be 1 or higher")).Where(x => int.IsEvenInteger(x)).ToArray();
     }
 
     /// <summary>
